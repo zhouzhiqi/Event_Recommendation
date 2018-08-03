40cd35be354db7d581beed01b8f593069827fcf5
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-08-03 22:30:56 +0800

    更改变量名与注释

--- _1d_events_distance.py
+++ _1d_events_distance.py
@@ -30,48 +30,53 @@ print('getting events information')
 with open(dpath+'events.csv') as events:
     # 读入列名信息
     columns = events.readline().split(',')
-    event_train_test_df=[]
+    event_info_df=[]
     for line in events:
         # 若读入的event_id在train/test中出现, 
-        # 添加入event_train_test_df
+        # 添加入event_info_df
         cols = line.split(',') 
         if int(cols[0]) in all_event:  
-            event_train_test_df.append(cols)
+            event_info_df.append(cols)
 
 print('to DataFrame')
-# 把生成好的event_train_test_df 转成DataFrame
-event_train_test = pd.DataFrame(event_train_test_df,columns=columns)
+# 把生成好的event_info_df 转成DataFrame
+event_info = pd.DataFrame(event_info_df,columns=columns)
 # 把空缺值替换为np.nan
-event_train_test.replace('',np.nan,inplace=True)
+event_info.replace('',np.nan,inplace=True)
 # 把event_id转换成index
-event_train_test.index = event_train_test.pop('event_id').apply(lambda x:events_index[int(x)])
+event_info.index = event_info.pop('event_id').apply(lambda x:events_index[int(x)])
 # 把user_id转换成int64
-event_train_test['user_id'] = event_train_test['user_id'].astype(np.int64)
+event_info['user_id'] = event_info['user_id'].astype(np.int64)
 # 把'start_time'转换成日期类型
-event_train_test['start_time'] = event_train_test['start_time'].astype(np.datetime64).dt.date
+event_info['start_time'] = event_info['start_time'].astype(np.datetime64).dt.date
 # 把'c_other\n'中所有的'\n'去掉
-event_train_test['c_other'] = event_train_test.pop('c_other\n').apply(lambda x:x[:-1]).values
+event_info['c_other'] = event_info.pop('c_other\n').apply(lambda x:x[:-1]).values
 
 print('Encoding . . . ')
 # 对类别特征进行LabelEncoder
 for c in ['city', 'state', 'country', 'lat', 'lng']:
-    event_train_test[c] = event_train_test[c].astype('category').values.codes
+    event_info[c] = event_info[c].astype('category').values.codes
 # 对'c_1', 'c_2', 'c_100', ***, 'c_other'进行归一化处理
-for c in event_train_test.columns[-101:]:
-    event_train_test[c] = event_train_test[c].astype(np.float64)
-    min_ = event_train_test[c].min()
-    max_ = event_train_test[c].max()
+for c in event_info.columns[-101:]:
+    event_info[c] = event_info[c].astype(np.float64)
+    min_ = event_info[c].min()
+    max_ = event_info[c].max()
     length_ = max_ - min_
-    event_train_test[c] = (event_train_test[c] - min_) / length_
+    event_info[c] = (event_info[c] - min_) / length_
 
+print('saving . . . ')
+# 保存处理好的event_info
+dump(event_info, tmp_dpath+'event_info.joblib.gz', compress=('gzip',3))
+
+"""
 print('getting the distance')
 # 生成数值型和类别型数据
-cat = event_train_test.loc[:, ['city', 'state', 'country', 'lat', 'lng']]
-num = event_train_test.loc[:, 'c_1':]
+cat = event_info.loc[:, ['city', 'state', 'country', 'lat', 'lng']]
+num = event_info.loc[:, 'c_1':]
 # 生成空的distance, 默认为1
 event_distance = np.ones((num_events, num_events), dtype=np.float64)
 # 生成event_index
-e_index = event_train_test.index
+e_index = event_info.index
 # 生成1/10的界限, 用于输出进度
 num_events010 = num_events//10
 for i,e1 in enumerate(e_index):
@@ -89,6 +94,6 @@ for i,e1 in enumerate(e_index):
 print('saving . . . ')
 # 保存处理好的event_distance
 dump(event_distance, tmp_dpath+'event_distance.joblib.gz', compress=('gzip',3))
-
-
+"""
+pass
 
